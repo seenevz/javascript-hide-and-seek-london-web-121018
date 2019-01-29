3490a4b29be89827cc11e253860d46b99c686536
seenevz <serneves@hotmail.com>
2019-01-29 15:46:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+};
 
+function nestedTarget() {
+    
+    return document.querySelector('#nested .target');
+};
+
+function deepestChild() {
+
+    return document.querySelector('div#grand-node div div div div');
+
+}
+
+function increaseRankBy(n) {
+    const ranks = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+
+    Array.from(ranks).forEach( lis => {
+        return lis.innerHTML = ((parseInt(lis.innerHTML) + n).toString())
+    })
+};
